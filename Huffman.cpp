8ca10aed8ab5cd52bc40d46d27dae1720320d3a8
Casoosted-Fuper <104692396+Casoosted-Fuper@users.noreply.github.com>
2025-04-26 14:41:34 +0000

    adding huffman.cpp stuff

---
+++ Huffman.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+#include <iostream>
+
+int main() {
+	string input = "";
+	//call the huffman function
+
+	//call the letterPair function
+}
+
+int frequency() {
+	//finding the frequency of each letter
+}
+
+string huffman() {
+	//creating the tree of nodes for the original huffman algorithm
+
+}
+
+string letterPair() {
+	//creating the tree of nodes for the letter pair algorithm
+
+}
