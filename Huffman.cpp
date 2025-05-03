84270c4cfa507dfe135ef7ba7f79bd5ba7d15f8b
Casoosted-Fuper <104692396+Casoosted-Fuper@users.noreply.github.com>
2025-05-03 12:52:09 +0000

    ITS WORKING!!!!

--- Huffman.cpp
+++ Huffman.cpp
@@ -91,18 +91,27 @@ vector<pair<int, char>> letterFrequency(const string& str) {
 }
 
 
-void doOutput(map<char, string> EncodedList) {
+void doOutput(map<char, string> EncodedList, string inputMessage) {
     ofstream outputFile;
     outputFile.open("EncodedOutput.txt");
 
     if (outputFile.is_open()) {
+        
         outputFile << "Encoded Output" << endl;
         outputFile << "----------------" << endl;
 
         for(pair<char, string> curr : EncodedList) {
+            //outputs each letter and its encoding
             outputFile << curr.first << ": " << curr.second << endl;
         }
-        // Write the encoded data here
+
+        outputFile << "----------------" << endl;
+        outputFile << "Full Encoded message:";
+        for(char ch : inputMessage) {
+            if (isalpha(ch)) {
+                outputFile << EncodedList[ch];
+            }
+        }
 
     } else {
         cout << "Unable to open file";
@@ -111,24 +120,19 @@ void doOutput(map<char, string> EncodedList) {
     outputFile.close();
 }
 
-void findEncoding(TreeNode& treeToUse, map<char, string>& EncodedList) {
-    TreeNode* root = &treeToUse;
-    TreeNode* currentNode = root;
-    string currFoundEncoding = "";
-
-    if (currentNode == NULL ){
-       return;
+void findEncoding(TreeNode* currentNode, string path, map<char, string>& EncodedList) {
+    if (currentNode == NULL) {
+        return;
     }
 
-    EncodedList[currentNode->NodeChar] += currentNode->NodeCode;
+    //Checking if its a leaf node
+    if (currentNode->left == NULL && currentNode->right == NULL) {
+        EncodedList[currentNode->NodeChar] = path;
+    }
 
-    //currFoundEncoding += currentNode->NodeCode;
-
-    findEncoding(*currentNode->left, EncodedList);
-
-    findEncoding(*currentNode->right, EncodedList);
-
-
+    //recursively calling the function while adding the current code to the path variable for each letter
+    findEncoding(currentNode->left, path + "1", EncodedList);
+    findEncoding(currentNode->right, path + "0", EncodedList);
 }
 
 void buildTree(priority_queue<TreeNode*, vector<TreeNode*>, TreeNodeCompare>& pqueue) {
@@ -141,9 +145,6 @@ void buildTree(priority_queue<TreeNode*, vector<TreeNode*>, TreeNodeCompare>& pq
     int totalVal = node1->NodeFrequency + node2->NodeFrequency;
 
     pqueue.push(newCombineNode(totalVal, node1, node2));
-
-    node1->NodeCode = "1";
-    node2->NodeCode = "0";
 }
 
 
@@ -151,35 +152,34 @@ void huffman() {
 	//creating the tree of nodes for the original huffman algorithm
     string OGFileString = readInFile();
     vector<pair<int, char>> OGStringFrequency = letterFrequency(OGFileString);
-    map<char, string> EncodingList;
-
-    for(int i = 0 ; i < 26 ;i++){
-        EncodingList.insert({'a'+i, ""});
-    }
+    map<char, string> EncodingList;    
 
     //create a priority queue
     priority_queue<TreeNode*, vector<TreeNode*>, TreeNodeCompare> max_priority_queue;
 
 
     for (pair<int, char> n : OGStringFrequency) {
-        max_priority_queue.push(newNode(n.first, n.second));
+        //If the letter has a frequency greater than zero,
+        //add it to the priority queue
+        if (n.first > 0) {
+            max_priority_queue.push(newNode(n.first, n.second));
+        }
     }
 
     while (max_priority_queue.size() > 1) {
         buildTree(max_priority_queue);
     }
 
-    findEncoding(*max_priority_queue.top(), EncodingList);
+    TreeNode* root = max_priority_queue.top();
+    findEncoding(root, "", EncodingList);
 
-    doOutput(EncodingList);
+    doOutput(EncodingList, OGFileString);
 
     //MEETING WITH ALEXI NOTES
 
-    //Need to generate a List to storew all the encodings. 
     //Need to find a way to do tree traversal to get the encodings
     //Tree traversal can look at the left and right children of the node and keep a current variable that can be appended to and then once the actuial node 
     //is found, then have to store the encoding in the list. 
-    //Dont worry about the Two Character version at this point. 
     //Finally print the encoding and then print the ecoded version and the key to the encoding.
     //Rewrite the orginal file in encoded format, and also print out the key to the encoding.
     //Key and the encoded file in the same file. 
